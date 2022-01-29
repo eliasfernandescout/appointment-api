36d6ea500808ab5b491aa4c55d29b4abdccb7604
Elias Fernandes <eliasfernandescout@gmail.com>
2022-01-29 18:13:40 -0300

    chore: server connection and get test

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/customer', (request, response) => {
+    const customerData = {
+        name: 'Ragnar Lothbrook',
+        cpf: '144.767.890-00',
+        dataDeNascimento: '13/09/1991',
+    }
+
+    response.send(customerData)
+})
+
+app.listen(5002, () => {
+    console.log('🟠 Server started on port 5002')
+});
